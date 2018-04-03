19b8dc93fc952f59577b813edb005956ffe2f84e
ryanmaksymic <ryanmaksymic@gmail.com>
2018-04-02 21:41:21 -0400

    Created NewShiftViewController class and hooked up outlets to the three date pickers.

---
+++ StaffManagement/NewShiftViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NewShiftViewController.swift
+//  StaffManagement
+//
+//  Created by Ryan Maksymic on 2018-04-02.
+//  Copyright © 2018 Derek Harasen. All rights reserved.
+//
+
+import UIKit
+
+class NewShiftViewController: UIViewController {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var startTimePicker: UIDatePicker!
+    @IBOutlet weak var endTimePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
