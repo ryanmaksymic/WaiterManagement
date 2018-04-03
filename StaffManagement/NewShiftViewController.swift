d1ff7e217f8d2e6007a02c928a0d9b1ec41c09bc
ryanmaksymic <ryanmaksymic@gmail.com>
2018-04-02 23:34:21 -0400

    Add delegation pattern between ShiftsTableViewController and NewShiftViewController. NewShiftViewController: Tapping Save button creates new Shift object for current waiter. Cancel and Save buttons both dismiss the current view.

--- StaffManagement/NewShiftViewController.swift
+++ StaffManagement/NewShiftViewController.swift
@@ -13,13 +13,30 @@ class NewShiftViewController: UIViewController {
     @IBOutlet weak var startTimePicker: UIDatePicker!
     @IBOutlet weak var endTimePicker: UIDatePicker!
     
+    var delegate: ShiftsTableViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        endTimePicker.date = startTimePicker.date + TimeInterval(60*60*8)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func cancelNewShift(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveNewShift(_ sender: UIBarButtonItem) {
+        let restaurantManager = RestaurantManager.sharedManager() as! RestaurantManager
+        restaurantManager.newShift(from: startTimePicker.date, to: endTimePicker.date, for: delegate.waiter)
+        dismiss(animated: true, completion: nil)
+    }
+    
     
     /*
     // MARK: - Navigation
--- StaffManagement/StaffManagement-Bridging-Header.h
+++ StaffManagement/StaffManagement-Bridging-Header.h
@@ -2,5 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#import "RestaurantManager.h"
 #import "ViewController.h"
 #import "Waiter.h"
